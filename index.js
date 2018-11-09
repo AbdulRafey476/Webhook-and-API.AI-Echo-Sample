0cfb5b30e7f89b8c3711f05ab52378bf7094e328
abdulrafey476 <abdul.rafey476@hotmail.com>
2018-11-09 17:51:03 +0500

    updates

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const Request = require("request");
+var NodeGeocoder = require('node-geocoder');
+
+const now = new Date()
 
 const restService = express();
 
@@ -10,15 +14,18 @@ restService.use(
     extended: true
   })
 );
-
 restService.use(bodyParser.json());
 
-restService.post("/pray", function (req, res) {
-  if (req.body.result.parameters.PrayCall === "Azan") {
-    var speech = '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>'
+restService.post("/times", function (req, res) {
+  if (req.body.result.parameters.GetPrayerTimes) {
+    Request.get(`http://api.aladhan.com/v1/calendar?latitude=24.8667795&longitude=67.0311286&method=2&month=${now.getMonth()}&year=${now.getFullYear()}`, (err, response, body) => {
+      if (err) throw err;
+      let data = JSON.parse(body)
+      let timing = data.data[0].timings
+    });
   }
   return res.json({
-    speech: speech,
+    speech: timing,
     displayText: speech,
     source: "webhook-echo-sample"
   });
@@ -27,5 +34,5 @@ restService.post("/pray", function (req, res) {
 // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
 
 restService.listen(process.env.PORT || 8000, function () {
-  console.log("Server up and listening");
+  console.log("Server up and listening 8000");
 });

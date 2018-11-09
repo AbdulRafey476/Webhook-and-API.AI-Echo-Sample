266174ece9e6dd68c12eca4cf161064eb9dae86d
abdulrafey476 <abdul.rafey476@hotmail.com>
2018-11-09 19:08:53 +0500

    skdjfn

--- index.js
+++ index.js
@@ -18,28 +18,19 @@ restService.use(bodyParser.json());
 
 
 restService.post("/echo", function (req, res) {
-  if (req.body.result.parameters.PrayerTime === "Time") {
-    Request.get(`http://api.aladhan.com/v1/calendar?latitude=24.8667795&longitude=67.0311286&method=2&month=${now.getMonth()}&year=${now.getFullYear()}`, (err, response, body) => {
-      if (err) throw err;
-      else {
-        var data = JSON.parse(body)
-        var timing = data.data[0].timings
-        var str = String(timing)
-        return res.json({
-          speech: str,
-          displayText: str,
-          source: "webhook-echo-sample"
-        });
-      }
-    });
-  }
-  else {
-    return res.json({
-      speech: "Seems like some problem",
-      displayText: "Seems like some problem",
-      source: "webhook-echo-sample"
-    });
-  }
+  Request.get(`http://api.aladhan.com/v1/calendar?latitude=24.8667795&longitude=67.0311286&method=2&month=${now.getMonth()}&year=${now.getFullYear()}`, (err, response, body) => {
+    if (err) throw err;
+    else {
+      var data = JSON.parse(body)
+      var timing = data.data[0].timings
+      var str = timing.Fajr
+      return res.json({
+        speech: str,
+        displayText: str,
+        source: "webhook-echo-sample"
+      });
+    }
+  });
 });
 
 // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3

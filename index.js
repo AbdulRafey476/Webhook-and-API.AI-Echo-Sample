0f0d9f60750fe91b5e00f87eae305d0000534c08
abdulrafey476 <abdul.rafey476@hotmail.com>
2018-11-09 18:35:18 +0500

    skdjfn

--- index.js
+++ index.js
@@ -14,21 +14,32 @@ restService.use(
     extended: true
   })
 );
-restService.use(bodyParser.json());
+// restService.use(bodyParser.json());
+// Request.get(`http://api.aladhan.com/v1/calendar?latitude=24.8667795&longitude=67.0311286&method=2&month=${now.getMonth()}&year=${now.getFullYear()}`, (err, response, body) => {
+//   if (err) throw err;
+//   var data = JSON.parse(body)
+//   var timing = data.data[0].timings
 
-restService.post("/times", function (req, res) {
-  if (req.body.result.parameters.PrayerTime.toLowerCase() === "time") {
-    Request.get(`http://api.aladhan.com/v1/calendar?latitude=24.8667795&longitude=67.0311286&method=2&month=${now.getMonth()}&year=${now.getFullYear()}`, (err, response, body) => {
-      if (err) throw err;
-      var data = JSON.parse(body)
-      var timing = data.data[0].timings
-      return res.json({
-        speech: "timing",
-        displayText: "timing",
-        source: "webhook-echo-sample"
-      });
-    });
-  }
+// });
+
+
+// restService.post("/times", function (req, res) {
+//   if (req.body.result.parameters.PrayerTime.toLowerCase() === "time") {
+//   }
+// });
+
+restService.post("/echo", function (req, res) {
+  var speech =
+    req.body.result &&
+      req.body.result.parameters &&
+      req.body.result.parameters.PrayerTime
+      ? req.body.result.parameters.PrayerTime
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 });
 
 // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3

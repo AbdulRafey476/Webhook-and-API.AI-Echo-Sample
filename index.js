27fa559cddb04a1f304343fc50dff261282faf46
abdulrafey476 <abdul.rafey476@hotmail.com>
2018-11-09 18:15:57 +0500

    skdjfn

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/times", function (req, res) {
-  if (req.body.result.parameters.PrayerTime === "Time") {
+  if (req.body.result.parameters.PrayerTime.toLowerCase() === "time") {
     Request.get(`http://api.aladhan.com/v1/calendar?latitude=24.8667795&longitude=67.0311286&method=2&month=${now.getMonth()}&year=${now.getFullYear()}`, (err, response, body) => {
       if (err) throw err;
-      let data = JSON.parse(body)
-      let timing = data.data[0].timings
+      var data = JSON.parse(body)
+      var timing = data.data[0].timings
+      return res.json({
+        speech: "timing",
+        displayText: "timing",
+        source: "webhook-echo-sample"
+      });
     });
   }
-  return res.json({
-    speech: timing,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
 });
 
 // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3

e1c177fb8d5e2d4788ad4841514e0d94191a8ac5
abdulrafey476 <abdul.rafey476@hotmail.com>
2018-11-14 17:04:42 +0500

    location updates

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Request = require("request");
+const geoip = require('geoip-lite');
+const ip = require('ip');
+const PORT = process.env.PORT || 8000;
+const publicIp = require('public-ip');
 
 const now = new Date()
 
@@ -18,29 +22,26 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function (req, res) {
   if (req.body.result.parameters.PrayerTime === "Time") {
-    Request.get(`https://ipapi.co/json`, (err, response, loc) => {
-      if (err) throw err;
-      else {
-        var location = JSON.parse(loc)
-        Request.get(`http://api.aladhan.com/v1/calendar?latitude=${location.latitude}&longitude=${location.longitude}&method=2&month=${now.getMonth()}&year=${now.getFullYear()}`, (err, response, body) => {
-          if (err) throw err;
-          else {
-            var data = JSON.parse(body)
-            var timing = data.data[0].timings
-            var str = `<speak>Fajar time is ${timing.Fajr}, <break time="1s"/> Dhuhr time is ${timing.Dhuhr}, <break time="1s"/> Asaar time is ${timing.Asr}, <break time="1s"/> Maghrib time is ${timing.Maghrib} <break time="1s"/> and Esha time is ${timing.Isha}</speak>`;
-            var disStr = `Fajar time is ${timing.Fajr}, Dhuhr time is ${timing.Dhuhr}, Asaar time is ${timing.Asr}, Maghrib time is ${timing.Maghrib} and Esha time is ${timing.Isha}`;
-            return res.json({
-              speech: str,
-              displayText: disStr,
-              source: "webhook-echo-sample"
-            });
-          }
-        });
-      }
+    publicIp.v4().then(function (ip) {
+      let geo = geoip.lookup(ip);
+      Request.get(`http://api.aladhan.com/v1/calendar?latitude=${geo.ll[0]}&longitude=${geo.ll[1]}&method=2&month=${now.getMonth()}&year=${now.getFullYear()}`, (err, response, body) => {
+        if (err) throw err;
+        else {
+          var data = JSON.parse(body)
+          var timing = data.data[0].timings
+          var str = `<speak>Fajar time is ${timing.Fajr}, <break time="1s"/> Dhuhr time is ${timing.Dhuhr}, <break time="1s"/> Asaar time is ${timing.Asr}, <break time="1s"/> Maghrib time is ${timing.Maghrib} <break time="1s"/> and Esha time is ${timing.Isha}</speak>`;
+          var disStr = `Fajar time is ${timing.Fajr}, Dhuhr time is ${timing.Dhuhr}, Asaar time is ${timing.Asr}, Maghrib time is ${timing.Maghrib} and Esha time is ${timing.Isha}`;
+          return res.json({
+            speech: str,
+            displayText: disStr,
+            source: "webhook-echo-sample"
+          });
+        }
+      });
     });
   }
 });
 
-restService.listen(process.env.PORT || 8000, function () {
+restService.listen(PORT, function () {
   console.log("Server up and listening 8000");
 });

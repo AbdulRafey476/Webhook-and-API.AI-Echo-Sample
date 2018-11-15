04e65d90b635b257724243ff8a0306fc6d059ef3
abdulrafey476 <abdul.rafey476@hotmail.com>
2018-11-15 17:41:48 +0500

    5 times azansssss

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const Request = require("request");
 const PORT = process.env.PORT || 8000;
+const moment = require('moment')
 
 const now = new Date()
 
@@ -18,48 +19,48 @@ restService.use(bodyParser.json());
 
 
 restService.post("/echo", function (req, res) {
-  if (req.body.result.parameters.PrayerTime === "Time") {
-    if (req.body.result.parameters.date !== "") {
-      var day = req.body.result.parameters.date
-      Request.get(`http://api.aladhan.com/v1/calendar?latitude=40.730610&longitude=-73.935242&method=2&month=${now.getMonth() + 1}&year=${now.getFullYear()}`, (err, response, body) => {
-        if (err) throw err;
-        else {
-          var data = JSON.parse(body)
-          var timing;
-          for (var i = 0; i < data.data.length; i++) {
-            var apiDate = data.data[i].date.readable
-            if (apiDate.slice(0, 2) === day.slice(8)) {
-              timing = data.data[i].timings
-            }
+  if (req.body.result.parameters.PrayerTime === "Time" && req.body.result.parameters.date !== "") {
+    // if () {
+    var day = req.body.result.parameters.date
+    Request.get(`http://api.aladhan.com/v1/calendar?latitude=40.730610&longitude=-73.935242&method=2&month=${now.getMonth() + 1}&year=${now.getFullYear()}`, (err, response, body) => {
+      if (err) throw err;
+      else {
+        var data = JSON.parse(body)
+        var timing;
+        for (var i = 0; i < data.data.length; i++) {
+          var apiDate = data.data[i].date.readable
+          if (apiDate.slice(0, 2) === day.slice(8)) {
+            timing = data.data[i].timings
           }
-          var str = `<speak> (${day}) Fajar time is ${timing.Fajr}, <break time="1s"/> Dhuhr time is ${timing.Dhuhr}, <break time="1s"/> Asaar time is ${timing.Asr}, <break time="1s"/> Maghrib time is ${timing.Maghrib} <break time="1s"/> and Esha time is ${timing.Isha}</speak>`;
-          var disStr = `(${day}) Fajar time is ${timing.Fajr}, Dhuhr time is ${timing.Dhuhr}, Asaar time is ${timing.Asr}, Maghrib time is ${timing.Maghrib} and Esha time is ${timing.Isha}`;
-          return res.json({
-            speech: str,
-            displayText: disStr,
-            source: "Nodejs"
-          });
         }
-      });
-    }
+        var str = `<speak> (${day}) Fajar time is ${timing.Fajr}, <break time="1s"/> Dhuhr time is ${timing.Dhuhr}, <break time="1s"/> Asaar time is ${timing.Asr}, <break time="1s"/> Maghrib time is ${timing.Maghrib} <break time="1s"/> and Esha time is ${timing.Isha}</speak>`;
+        var disStr = `(${day}) Fajar time is ${timing.Fajr}, Dhuhr time is ${timing.Dhuhr}, Asaar time is ${timing.Asr}, Maghrib time is ${timing.Maghrib} and Esha time is ${timing.Isha}`;
+        return res.json({
+          speech: str,
+          displayText: disStr,
+          source: "Nodejs"
+        });
+      }
+    });
+    // }
 
-    else {
-      var day = req.body.result.parameters.date
-      Request.get(`http://api.aladhan.com/v1/calendar?latitude=40.730610&longitude=-73.935242&method=2&month=${now.getMonth() + 1}&year=${now.getFullYear()}`, (err, response, body) => {
-        if (err) throw err;
-        else {
-          var data = JSON.parse(body)
-          var timing = data.data[0].timings
-          var str = `<speak>Fajar time is ${timing.Fajr}, <break time="1s"/> Dhuhr time is ${timing.Dhuhr}, <break time="1s"/> Asaar time is ${timing.Asr}, <break time="1s"/> Maghrib time is ${timing.Maghrib} <break time="1s"/> and Esha time is ${timing.Isha}</speak>`;
-          var disStr = `Fajar time is ${timing.Fajr}, Dhuhr time is ${timing.Dhuhr}, Asaar time is ${timing.Asr}, Maghrib time is ${timing.Maghrib} and Esha time is ${timing.Isha}`;
-          return res.json({
-            speech: str,
-            displayText: disStr,
-            source: "Nodejs"
-          });
-        }
-      });
-    }
+    // else {
+    //   var day = req.body.result.parameters.date
+    //   Request.get(`http://api.aladhan.com/v1/calendar?latitude=40.730610&longitude=-73.935242&method=2&month=${now.getMonth() + 1}&year=${now.getFullYear()}`, (err, response, body) => {
+    //     if (err) throw err;
+    //     else {
+    //       var data = JSON.parse(body)
+    //       var timing = data.data[0].timings
+    //       var str = `<speak>Fajar time is ${timing.Fajr}, <break time="1s"/> Dhuhr time is ${timing.Dhuhr}, <break time="1s"/> Asaar time is ${timing.Asr}, <break time="1s"/> Maghrib time is ${timing.Maghrib} <break time="1s"/> and Esha time is ${timing.Isha}</speak>`;
+    //       var disStr = `Fajar time is ${timing.Fajr}, Dhuhr time is ${timing.Dhuhr}, Asaar time is ${timing.Asr}, Maghrib time is ${timing.Maghrib} and Esha time is ${timing.Isha}`;
+    //       return res.json({
+    //         speech: str,
+    //         displayText: disStr,
+    //         source: "Nodejs"
+    //       });
+    //     }
+    //   });
+    // }
   }
 
   else if (req.body.result.parameters.PrayerNames === "Fajar") {

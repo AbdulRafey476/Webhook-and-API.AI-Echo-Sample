5f7a7e8b96147bdd2fed9529f72a6f88c35b985f
abdulrafey476 <abdul.rafey476@hotmail.com>
2018-11-19 12:04:48 +0500

    asfs

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ const PORT = process.env.PORT || 8000;
 const functions = require('firebase-functions');
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 
+var firebase = require('firebase');
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDENTQQ3_TMvzF8nKTj59Ocxql-f1nRT4k",
+  authDomain: "prayercall-1d584.firebaseapp.com",
+  databaseURL: "https://prayercall-1d584.firebaseio.com",
+  projectId: "prayercall-1d584",
+  storageBucket: "prayercall-1d584.appspot.com",
+  messagingSenderId: "1052788735460"
+};
+firebase.initializeApp(config);
+
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://prayercall-1d584.firebaseio.com"
+});
+
+
 const restService = express();
 
 restService.use(
@@ -15,6 +39,38 @@ restService.use(
   })
 );
 restService.use(bodyParser.json());
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+
+  const requestPermission = (app) => {
+    app.askForPermission('To locate you', app.SupportedPermissions.DEVICE_PRECISE_LOCATION);
+  };
+
+  const userInfo = (app) => {
+    if (app.isPermissionGranted()) {
+      const address = app.getDeviceLocation().address;
+      if (address) {
+        app.tell(`You are at ${address}`);
+      }
+      else {
+        // Note: Currently, precise locaton only returns lat/lng coordinates on phones and lat/lng coordinates 
+        // and a geocoded address on voice-activated speakers. 
+        // Coarse location only works on voice-activated speakers.
+        app.tell('Sorry, I could not figure out where you are.');
+      }
+    } else {
+      app.tell('Sorry, I could not figure out where you are.');
+    }
+  };
+
+  const app = new DialogflowApp({ request, response });
+  const actions = new Map();
+  actions.set('request_permission', requestPermission);
+  actions.set('user_info', userInfo);
+  app.handleRequest(actions);
+
+});
+
 
 restService.post("/", function (req, res) {
 
